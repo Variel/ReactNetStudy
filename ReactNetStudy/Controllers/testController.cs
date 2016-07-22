258b1dfe0c59e8d834638fee6fa6dedd662e4de0
김영근 <iceflower01@gmail.com>
2016-07-22 16:24:28 +0900

    ㅁㅇㄻㅇㄻㄴㅇㄹ

---
+++ ReactNetStudy/Controllers/testController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReactNetStudy.Controllers
+{
+    public class testController : Controller
+    {
+        // GET: test
+        public ActionResult Index()
+        {
+
+            //앙 컨트롤띠
+            return View();
+        }
+    }
+}
